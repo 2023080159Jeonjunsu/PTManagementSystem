b32507ffd0bd0091f4333c95bc56c851b5bcf277
Jun su Jeon <jjc224@naver.com>
2023-03-22 10:00:20 +0900

    added Menu.java

---
+++ src/menu/Menu.java
@@ -0,0 +1,12 @@
+package menu;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add PT-Members");
+		System.out.println("2. Delete PT-Members");
+		System.out.println("3. Edit PT-Members");
+		System.out.println("4. View PT-Members");
+		System.out.println("5. Exit");
+	}
+}
